b68d0db9ca823417e764a8ba3ee288c17e77b5c9
Jon Lu <jontlu@me.com>
2015-07-24 10:34:55 -0400

    without redundancy

--- app.py
+++ app.py
@@ -1,8 +1,5 @@
 from flask import Flask, render_template, request
-from util import *
 import datetime
-import urllib2
-from urllib import urlencode
 
 if __name__ == '__main__':
     app.debug = True
