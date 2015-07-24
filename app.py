7f5f30069f44038d6f183ea9ccdda9b6b73723ec
Jon Lu <jontlu@me.com>
2015-07-24 10:36:10 -0400

    should work

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
 from flask import Flask, render_template, request
 import datetime
 
-if __name__ == '__main__':
-    app.debug = True
-    app.run( port = 4269 )
-
 app = Flask( __name__ )
 
 @app.route('/', methods=["GET","POST"])
 def main():
     return render_template('index.html')
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run( port = 4269 )
+
