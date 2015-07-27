5ff144156760359c726d01d0a6232a9c427c4d56
Jon Lu <jontlu@me.com>
2015-07-27 11:18:50 -0400

    .__.

--- app.py
+++ app.py
@@ -6,14 +6,8 @@ app = Flask( __name__ )
 
 @app.route('/', methods=["GET","POST"])
 def main():
-    return render_template('base.html')
+    return render_template('base.html', inputTXT=returnTXT())
     
-@app.route('/', methods=["POST"])
-def text():
-    inputTXT = request.form['asd']
-    return inputTXT
-    
-
 if __name__ == '__main__':
     app.debug = True
     app.run( port = 4269 )
