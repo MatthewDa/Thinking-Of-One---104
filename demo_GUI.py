00689e04f0fed7362fe40b3aaec6aab256513fda
Jon Lu <jontlu@me.com>
2015-07-27 08:55:53 -0400

    now with ultimate console powers

--- demo_GUI.py
+++ demo_GUI.py
@@ -1,11 +1,10 @@
-import pyautogui
+import typewrite as pyautogui
 
 def demo(uinput):
     item = uinput
     pyautogui.typewrite('Hello, traveler.', interval=0.1)
     pyautogui.press('enter', interval=1)
     pyautogui.typewrite('I see you want ' + item + '.', interval=.25)
-##        return 'Pressed Enter?'
 
 def prompts():
     pyautogui.alert('This displays some text with an OK button.')
